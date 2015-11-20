37e31749f74dc3214103734457a4da2687723b07
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-20 12:08:33 -0700

    chss

---
+++ chapter_02/chess_board.js
@@ -0,0 +1,35 @@
+/*
+Chess board
+
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+*/
+
+
+function board(n) {
+  var checks = ['.', '#'];
+  var row = '';
+  for (var j = 1; j <= n; j++) {
+    for (var i = 1; i <= n; i ++) {
+      if (i % 2) row += checks[0];
+      else row += checks[1];
+    }
+    row += '\n';
+    checks.push(checks[0]);
+    checks.splice(0,1);
+  }
+  console.log(row);
+}
+
+board(8);
